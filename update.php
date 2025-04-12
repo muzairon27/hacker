6d0f0210c62835c2e34af29404127c72c7db0bda
muzairon27 <muhammaduzairon27@gmail.com>
2025-04-12 12:55:38 +0700

    Add files via upload

---
+++ update.php
@@ -0,0 +1,24 @@
+<?php
+if(isset($_POST['update'])) {
+    $nis = $_POST['nis'];
+    $nama = $_POST ['nama'];
+    $jk = $_POST ['jk'];
+    $tempat_lahir = $_POST ['tempat_lahir'];
+    $tanggal_lahir = $_POST ['tanggal_lahir'];
+    $alamat = $_POST ['alamat'];
+    $telp= $_POST ['telp'];
+    $email = $_POST ['email'];
+    $jurusan_id = $_POST ['jurusan_id'];
+
+    include_once "setting.php";
+    $sql = "UPDATE siswa SET nis='$nis' , nama='$nama', jk='$jk', tempat_lahir='$tempat_lahir', tanggal_lahir='$tanggal_lahir',
+    alamat='$alamat', telp='$telp', email='$email', jurusan_id='$jurusan_id' WHERE nis='$nis'";
+    $result =mysqli_query($koneksi, $sql);
+    if($result) {
+        header ('location: ?m=siswa&s=tampil');
+    } else {
+        echo "Error: " . $sql . "<br>" . $koneksi->error;
+    }
+} else {
+    echo "Jangan Akses Langsung ke file simpan.php";
+}
